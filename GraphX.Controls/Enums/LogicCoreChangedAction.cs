32b13b02e4a057b8d9111fad98730f90ab024228
Alexander Smirnov <panthernet@mail.ru>
2015-02-27 15:34:48 +0300

    Added missing files

---
+++ GraphX.Controls/Enums/LogicCoreChangedAction.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphX.Controls.Enums
+{
+    public enum LogicCoreChangedAction
+    {
+        None = 0,
+        GenerateGraph,
+        GenerateGraphWithEdges,
+        RelayoutGraph,
+        RelayoutGraphWithEdges
+    }
+}
---
+++ GraphX.METRO.Controls/Enums/LogicCoreChangedAction.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphX.METRO.Controls.Enums
+{
+    public enum LogicCoreChangedAction
+    {
+        None = 0,
+        GenerateGraph,
+        GenerateGraphWithEdges,
+        RelayoutGraph,
+        RelayoutGraphWithEdges
+    }
+}
