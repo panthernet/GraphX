97419fef5627e27fc7613b64749af3f8c09c543a
perturbare <perturbare@users.noreply.github.com>
2015-05-20 13:45:19 -0700

    Added stream I/O support
    Added stream I/O support to FileServiceProviderWPF. (Still need changes
    for METRO projects)

--- Examples/ShowcaseApp.WPF/FileSerialization/FileServiceProviderWPF.cs
+++ Examples/ShowcaseApp.WPF/FileSerialization/FileServiceProviderWPF.cs
@@ -19,21 +19,48 @@ namespace ShowcaseApp.WPF.FileSerialization
         /// <param name="modelsList">Data classes list</param>
         public static void SerializeDataToFile(string filename, List<GraphSerializationData> modelsList)
         {
-            var serializer = new YAXSerializer(typeof(List<GraphSerializationData>));
-            using (var textWriter = new StreamWriter(filename))
+            using (FileStream stream = File.Open(filename, FileMode.Create, FileAccess.Write, FileShare.Read))
             {
-                serializer.Serialize(modelsList, textWriter);
-                textWriter.Close();
+                SerializeDataToStream(stream, modelsList);
             }
         }
+
         /// <summary>
         /// Deserializes data classes list from file
         /// </summary>
         /// <param name="filename">File name</param>
-        public static List<GraphSerializationData> DeserializeDataFromFile(string filename)
+		public static List<GraphSerializationData> DeserializeDataFromFile(string filename)
+        {
+            using (FileStream stream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return DeserializeDataFromFile(stream);
+            }
+        }
+
+        /// <summary>
+        /// Serializes graph data list to a stream
+        /// </summary>
+        /// <param name="stream">The destination stream</param>
+        /// <param name="modelsList">The graph data</param>
+		public static void SerializeDataToStream(Stream stream, List<GraphSerializationData> modelsList)
+        {
+            var serializer = new YAXSerializer(typeof(List<GraphSerializationData>));
+            using (var textWriter = new StreamWriter(stream))
+            {
+                serializer.Serialize(modelsList, textWriter);
+                textWriter.Flush();
+            }
+        }
+
+        /// <summary>
+        /// Deserializes graph data from a stream
+        /// </summary>
+        /// <param name="stream">The stream</param>
+        /// <returns>The graph data</returns>
+		public static List<GraphSerializationData> DeserializeDataFromFile(Stream stream)
         {
             var deserializer = new YAXSerializer(typeof(List<GraphSerializationData>));
-            using (var textReader = new StreamReader(filename))
+            using (var textReader = new StreamReader(stream))
             {
                 return (List<GraphSerializationData>)deserializer.Deserialize(textReader);
             }
