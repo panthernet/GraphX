e2bc58e8e6f3377a1a058ce1d629f896e306ef86
panthernet <panthernet@mail.ru>
2015-09-04 03:10:04 +0300

    + fixed missing file error

---
+++ GraphX.PCL.Logic/Helpers/ReflectionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraphX.PCL.Logic.Helpers
+{
+    public static class ReflectionHelper
+    {
+        public static T CreateDefaultGraphInstance<T>()
+        {
+            return (T)Activator.CreateInstance(typeof(T), null);
+        }
+    }
+}
