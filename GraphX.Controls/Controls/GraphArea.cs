5235639c259fa10667f5bc2f7f376ec63dbfb083
bleibold <autosoft@autosofttech.com>
2018-03-24 22:09:36 -0600

    Update GraphArea.cs
    Testing revealed an issue.

--- GraphX.Controls/Controls/GraphArea.cs
+++ GraphX.Controls/Controls/GraphArea.cs
@@ -1740,11 +1740,11 @@ namespace GraphX.Controls
             if (ec == null) return list;
             
             var edge = (TEdge)ec.Edge;
-            if (_vertexlist.ContainsKey(edge.Target) && edge.Target != null)
+            if (edge.Target != null && _vertexlist.ContainsKey(edge.Target))
             {
                 list.Add(_vertexlist[edge.Target]);
             }
-            if (_vertexlist.ContainsKey(edge.Source) && edge.Source != null)
+            if (edge.Source != null && _vertexlist.ContainsKey(edge.Source))
             {
                 list.Add(_vertexlist[edge.Source]);
             }
